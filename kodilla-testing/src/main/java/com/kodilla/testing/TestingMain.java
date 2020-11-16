61a107e93e0602dfe6ff67dcb09e515af25caeef
Mateusz Kwiatanowski <mateusz.kwiatanowski@gmail.com>
2020-11-16 10:31:33 +0100

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test user name is OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(5,2);
+
+        int resultAdd = calculator.add();
+        int resultSubtract = calculator.subtract();
+
+        if (resultAdd == calculator.getA() + calculator.getB()){
+
+            System.out.println("Test add is ok");
+
+        } else {
+
+            System.out.println("Error");
+        }
+
+        if (resultSubtract == calculator.getA() - calculator.getB()){
+
+            System.out.println("Test subtract is ok");
+
+        } else {
+
+            System.out.println("Error");
+
+        }
+
     }
+
 }
