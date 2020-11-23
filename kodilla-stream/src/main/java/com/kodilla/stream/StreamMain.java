c946798f711974edb2628664c92dfae976e2b05e
Mateusz Kwiatanowski <mateusz.kwiatanowski@gmail.com>
2020-11-23 09:04:22 +0100

    Stream forum user

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -53,6 +54,11 @@ public class StreamMain {
                return textPartOne+textPartTwo;
 
           });
+
+          System.out.println("Using Stream to generate even numbers from 1 to 20");
+          NumbersGenerator.generateEven(20);
+
+
 
      }
 }
